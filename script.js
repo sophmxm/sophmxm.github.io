0fbc5fac9f8e5855c2e320389575f640c0830698
sophmxm <65217607+sophmxm@users.noreply.github.com>
2025-06-26 14:18:39 +1000

    Add reveal animation on page load

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ console.log(main_sections);
 
 // Create the navigation section dots
 function createNavDots() {
-    // For each section in main, create navigation dot
+	// For each section in main, create navigation dot
 	for (let i = 0; i < main_sections.length; i++) {
 		let dot = document.createElement("div");
 		dot.classList.add("dot");
 		nav_dots.appendChild(dot);
 
-        // Add element to array
+		// Add element to array
 		nav_dots_list.push(dot);
 	}
 }
 
 // Set active navigation section dot
 function setActiveNavDot() {
-    // Check each section
+	// Check each section
 	for (let i = 0; i < main_sections.length; i++) {
 		// Check if scroll is within bounds of one of the sections and set active style
 		if (main_sections[i].offsetTop < main.scrollTop + main.offsetHeight / 4 && main_sections[i].offsetTop + main_sections[i].offsetHeight > main.scrollTop + main.offsetHeight / 4) {
-            // Remove style from previous active dot
+			// Remove style from previous active dot
 			nav_dots_list.forEach((dot) => dot.classList.remove("active"));
-            // Add style to current active dot
+			// Add style to current active dot
 			nav_dots_list[i].classList.add("active");
 		}
 	}
@@ -37,5 +37,17 @@ function setActiveNavDot() {
 // Set active navigation dot on scroll event
 main.addEventListener("scroll", setActiveNavDot);
 
+// Staggered reveal content effect
+function revealContent() {
+	for (let i = 0; i < main.children.length; i++) {
+		console.log(main.children[i].children)
+		for (let j = 0; j < main.children[i].children.length; j++) {
+			// Add animation delay effect to each section in content
+			main.children[i].children[j].style.animationDelay = `calc(350ms + ${j * 150}ms)`;
+		}
+	}
+}
+
 createNavDots();
 setActiveNavDot();
+revealContent();
