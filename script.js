2fb1748c9168b4a48727491c9393007d6a2b886b
sophmxm <65217607+sophmxm@users.noreply.github.com>
2026-01-24 21:52:07 +1000

    Add footer style and script

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-"use strict"
+"use strict";
 
 createHeader();
+createFooter();
 
 function createHeader() {
-    const header = document.getElementsByTagName("header")[0];
+	const header = document.getElementsByTagName("header")[0];
 
-    const header_items = [["Sophie Martin", "/"], ["My stuff", "#"], ["About me", "#"], ["Say hi", "#"]];
+	const header_items = [
+		["Sophie Martin", "/"],
+		["My stuff", "#"],
+		["About me", "#"],
+		["Say hi", "#"],
+	];
 
-    let nav = document.createElement("nav");
-    header.appendChild(nav);
+	let nav = document.createElement("nav");
+	header.appendChild(nav);
 
-    let list = document.createElement("ul");
-    nav.appendChild(list);
+	let list = document.createElement("ul");
+	nav.appendChild(list);
 
-    header_items.forEach(item => {
-        let list_item = document.createElement("li");
-        list.appendChild(list_item);
+	header_items.forEach((item) => {
+		let list_item = document.createElement("li");
+		list.appendChild(list_item);
 
-        let link = document.createElement("a");
-        link.innerText = item[0];
-        link.href = item[1];
-        list_item.appendChild(link);
-    })
+		let link = document.createElement("a");
+		link.innerText = item[0];
+		link.href = item[1];
+		list_item.appendChild(link);
+	});
+}
+
+function createFooter() {
+	const footer = document.getElementsByTagName("footer")[0];
+
+    let footer_container = document.createElement("div");
+    footer.appendChild(footer_container);
+
+	let p = document.createElement("p");
+	footer_container.appendChild(p);
+
+	let link_contact = document.createElement("a");
+	link_contact.innerText = "Contact me";
+	link_contact.href = "#";
+
+	let link_resume = document.createElement("a");
+	link_resume.innerText = "download my resume";
+	link_resume.href = "#";
+
+	p.innerText = "Come say hi! ";
+	p.appendChild(link_contact);
+	p.append(" or ");
+	p.appendChild(link_resume);
 }
