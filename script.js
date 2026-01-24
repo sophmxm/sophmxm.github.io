3a74a78110446d67d25d9d2cd60006e735335a93
sophmxm <65217607+sophmxm@users.noreply.github.com>
2026-01-24 22:22:34 +1000

    Add social links script and style

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 "use strict";
 
+const social_links = [
+	{ type: "email", text: "sophmxm@gmail.com", link: "mailto:sophmxm@gmail.com", icon: "ic:round-email" },
+	{ type: "linkedin", text: "linkedin.com/sophmxm", link: "https://www.linkedin.com/in/sophmxm/", icon: "mdi:linkedin" },
+];
+
 createHeader();
 createFooter();
+createSocialIconsList();
 
 function createHeader() {
 	const header = document.getElementsByTagName("header")[0];
 
-	const header_items = [
-		["Sophie Martin", "/"],
-		["My stuff", "#"],
-		["About me", "#"],
-		["Say hi", "#"],
+	const nav_links = [
+		{ text: "Sophie Martin", link: "/" },
+		{ text: "My stuff", link: "#" },
+		{ text: "About me", link: "#" },
+		{ text: "Say hi", link: "#" },
 	];
 
 	let nav = document.createElement("nav");
@@ -19,13 +25,13 @@ function createHeader() {
 	let list = document.createElement("ul");
 	nav.appendChild(list);
 
-	header_items.forEach((item) => {
+	nav_links.forEach((item) => {
 		let list_item = document.createElement("li");
 		list.appendChild(list_item);
 
 		let link = document.createElement("a");
-		link.innerText = item[0];
-		link.href = item[1];
+		link.innerText = item.text;
+		link.href = item.link;
 		list_item.appendChild(link);
 	});
 }
@@ -33,8 +39,8 @@ function createHeader() {
 function createFooter() {
 	const footer = document.getElementsByTagName("footer")[0];
 
-    let footer_container = document.createElement("div");
-    footer.appendChild(footer_container);
+	let footer_container = document.createElement("div");
+	footer.appendChild(footer_container);
 
 	let p = document.createElement("p");
 	footer_container.appendChild(p);
@@ -51,4 +57,27 @@ function createFooter() {
 	p.appendChild(link_contact);
 	p.append(" or ");
 	p.appendChild(link_resume);
+
+	let social_icons_list = document.createElement("ul");
+	social_icons_list.classList.add("social_icons_list");
+	footer_container.appendChild(social_icons_list);
+}
+
+function createSocialIconsList() {
+	let social_icons_list_elements = document.querySelectorAll(".social_icons_list");
+
+	social_icons_list_elements.forEach((list) => {
+		social_links.forEach((item) => {
+			let list_item = document.createElement("li");
+			list.appendChild(list_item);
+
+			let link = document.createElement("a");
+			link.href = item.link;
+			list_item.appendChild(link);
+
+			let icon = document.createElement("iconify-icon");
+			icon.icon = item.icon;
+			link.appendChild(icon);
+		});
+	});
 }
