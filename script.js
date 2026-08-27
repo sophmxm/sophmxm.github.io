7124099c16b697164980d85e09b8766f7dcc755b
sophmxm <65217607+sophmxm@users.noreply.github.com>
2026-08-27 10:52:01 +1000

    Update CV to v3

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ function createFooter() {
 	let link_resume = document.createElement("a");
 	link_resume.innerText = "download my resume";
 	link_resume.target = "_blank";
-	link_resume.href = "/resources/documents/CV.pdf";
+	link_resume.href = "/resources/documents/CV v3 3.pdf";
 
 	p.innerText = "Come say hi! ";
 	p.appendChild(link_contact);
